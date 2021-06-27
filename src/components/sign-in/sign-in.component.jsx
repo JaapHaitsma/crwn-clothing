c9527c1821a3ed929603809d9ba8b4404eef07b5
Jaap A. Haitsma <jaap@haitsma.org>
2021-06-26 22:59:00 -0400

    lesson 118

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 import './sign-in.styles.scss';
 
@@ -20,15 +20,14 @@ class SignIn extends React.Component {
   handleSubmit = async event => {
     event.preventDefault();
 
-    const {email, password} = this.state;
+    const { email, password } = this.state;
 
     try {
       await auth.signInWithEmailAndPassword(email, password);
       this.setState({ email: '', password: '' });
     } catch(error) {
       console.log(error);
-    }
-    
+    }    
   };
 
   handleChange = event => {
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -21,7 +21,7 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   const snapShot = await userRef.get();
 
   if (!snapShot.exists) {
-    const { displayName, email} = userAuth;
+    const { displayName, email } = userAuth;
     const createdAt = new Date();
     try {
       await userRef.set({
@@ -30,19 +30,18 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
         createdAt,
         ...additionalData
       }); 
-
     } catch (error) {
-      console.log('error creating user', error);
+      console.log('error creating user', error.message);
     }
   }
+
   return userRef;
-}
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-
 provider.setCustomParameters({ prompt: 'select_account' });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
